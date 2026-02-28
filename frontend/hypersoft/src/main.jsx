70cddb8547961ea647a2c69b0c28408ab420a7dd
GabrielMMMMM <gabrielmmarcos7@gmail.com>
2026-02-28 13:40:32 -0300

    feat(navbar) creating navbar component

--- frontend/hypersoft/src/main.jsx
+++ frontend/hypersoft/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+//login
+import Login from "./pages/Login";
+
+const router = createBrowserRouter([
+  //rotas login
+  {
+    path: "/",
+    element: <Login />,
+  },
+
+
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
