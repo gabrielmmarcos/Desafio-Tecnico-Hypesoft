3823f66ba04cae0e3ca369fa5a9860d90ae2de6b
GabrielMMMMM <gabrielmmarcos7@gmail.com>
2026-03-01 17:28:23 -0300

    style(login) fix login style

--- frontend/hypersoft/src/pages/login/index.jsx
+++ frontend/hypersoft/src/pages/login/index.jsx
@@ -139,7 +139,7 @@ function Login() {
                             type="submit"
                             className="bg-purple-600 hover:bg-purple-700 transition text-white font-semibold py-3 rounded-lg mt-4 shadow-lg cursor-pointer"
                         >
-                            Criar Conta
+                            Entrar
                         </button>
 
                         <button
